39aa700827a649f16f56fab662df4747d9b756b3
Okoye Victor <okoyeebuka64@gmail.com>
2022-05-01 23:54:16 +0100

    Updated Category

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 
+const categories= ['fruit', 'vegetables', 'dairy'];
 
 
 app.get('/products', async (req, res) => {
@@ -33,7 +34,7 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { products })
 })
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', {categories} )
 })
 app.post('/products', async (req, res) => {
     const newProduct = new Product(req.body);
@@ -52,7 +53,7 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product })
+    res.render('products/edit', { product, categories })
 })
 
 app.put('/products/:id', async (req, res) => {
